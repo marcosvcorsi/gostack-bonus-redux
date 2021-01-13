9dd77a9f83ca5224e5f53cbce6e196b5476f8bf8
Marcos Corsi <marcosvcorsi@gmail.com>
2021-01-12 21:47:41 -0300

    feat: add types to redux state and cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { IState } from '../../store';
+import cart from '../../store/modules/cart/reducer';
+import { ICartItem } from '../../store/modules/cart/types';
 
 const Cart: React.FC = () => {
-  const state = useSelector(state => state);
+  const items = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
-  console.log(state);
+  console.log(cart);
 
   return (
     <table>
@@ -16,7 +19,14 @@ const Cart: React.FC = () => {
           <th>Subtotal</th>
         </tr>
         <tbody>
-
+          {items.map(item => (
+            <tr key={item.product.id}>
+              <td>{item.product.title}</td>
+              <td>{item.product.price}</td>
+              <td>{item.quantity}</td>
+              <td>{(item.quantity * item.product.price).toFixed(2)}</td>
+            </tr>
+          ))}
         </tbody>
       </thead>
     </table>
